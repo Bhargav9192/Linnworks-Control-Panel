d3c103ffb0b15ec0ff575473937b32733e30fe9b
Bhargav Gadhadara <bhargav.gadhadara@rishvi.co.uk>
2026-03-05 10:31:20 +0000

    Make change for Logs add

--- LinnworksMacroHelpers/Classes/LoggingConfig.cs
+++ LinnworksMacroHelpers/Classes/LoggingConfig.cs
@@ -1,13 +1,28 @@
 ﻿using Elasticsearch.Net;
 using Serilog;
 using Serilog.Sinks.Elasticsearch;
+using Serilog.Debugging;
 using System;
 
 public static class LoggingConfig
 {
     public static void Configure()
     {
-        var elasticUrl = "http://77.68.17.136:9200";
+        // Allow overriding the Elasticsearch URL from environment (useful when hosted)
+        var elasticUrl = Environment.GetEnvironmentVariable("ELASTIC_URL") ?? "http://77.68.17.136:9200";
+
+        // Enable Serilog internal/self logging so failures from sinks are visible in host logs
+        SelfLog.Enable(msg => {
+            try
+            {
+                // Write to console so platform log collectors (e.g. Railway) will capture it
+                Console.Error.WriteLine("Serilog-SelfLog: " + msg);
+            }
+            catch
+            {
+                // swallow - best effort only
+            }
+        });
 
         Log.Logger = new LoggerConfiguration()
             .MinimumLevel.Information()
@@ -16,7 +31,10 @@ public static class LoggingConfig
             .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(elasticUrl))
             {
                 AutoRegisterTemplate = true,
-                IndexFormat = "linnworks-simulator-logs-{0:yyyy.MM.dd}"
+                IndexFormat = "linnworks-simulator-logs-{0:yyyy.MM.dd}",
+                // When emitting fails, write failures to Serilog SelfLog so they appear in host logs
+                EmitEventFailure = EmitEventFailureHandling.WriteToSelfLog,
+                FailureCallback = (logEvent, ex) => SelfLog.WriteLine($"Elasticsearch sink failure: {ex?.Message}")
             })
             .CreateLogger();
     }
